fb2e380baed04bb1f7f79723458762840febaf6b
Prof. Celso <celsofurtado@terra.com.br>
2022-02-17 21:54:51 -0300

    Live 17-02-2022 - 4SCJO

---
+++ app/src/main/java/br/com/fiap/imc4scjo/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.imc4scjo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
